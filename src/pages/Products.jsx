5981f738df02bedbfdfbf25a207c0eadfcd5d75d
juan pablo romero <jpr.1992@hotmail.com>
2023-10-13 22:27:21 -0300

    carrito full + input beta

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -15,14 +15,8 @@ const Products = () => {
   const [selectedSubcategory, setSelectedSubcategory] = useState('');
   const [sortOrder, setSortOrder] = useState('asc');
   const { allItems,category,subcategories } = useSelector((store) => store.itemsReducer);
+  const { cart } = useSelector((store) => store.cartReducer);
   const dispatch = useDispatch()
-  
-  useEffect(() => {
-    if (allItems.length === 0) {
-      dispatch(allProducts());
-      dispatch(allCategories());
-    }
-  }, []);
   
   const filteredProducts = allItems
     .filter((product) => {
@@ -48,7 +42,6 @@ const Products = () => {
     // console.log(filteredProducts,"tuhermana");
   const handleFavorite = (id)=>{
     const favorite = allItems.find(item => item._id === id)
-    console.log(favorite);
     dispatch(actionCart(favorite))
   }
 
@@ -114,13 +107,20 @@ const Products = () => {
         {filteredProducts.length > 0 ? (
           <div className='cardcontainer md:w-[75vw] min-h-3/4  flex flex-wrap p-2 items-center justify-center gap-5 md:my-5'>
             {filteredProducts.map((product) => (
-              <div key={product._id} className='h-72 w-36 flex flex-col border border-[#053b50] rounded-md gap-2 items-center md:w-52 md:h-80 justify-between md:hover:scale-110 md:transform md:duration-300 shadow-gray-600 shadow-lg bg-[#0e4355b2] p-1 '>
-                <img className='h-6 fixed top-0 right-1' onClick={()=>handleFavorite(product?._id)} src={favorite} alt="favorite " />
+              <div key={product._id} className='h-72 w-36 flex flex-col border border-[#053b50] rounded-md gap-2 items-center md:w-52 md:h-80 justify-between md:hover:scale-105 md:transform md:duration-300 shadow-gray-600 shadow-lg bg-[#0e4355b2] p-1 '>
+                <img className='h-6 fixed top-0 right-1'  src={favorite} alt="favorite " />
                 <h3 className='text-xs text-white font-bold pb-2'>{product.name}</h3>
                 <img className='w-full h-20 bg-white border md:w-full md:h-40 md:object-contain' src={product.photo} alt="" />
                 <p className='text-white text-xs line-clamp-3'>{product.description}</p>
                 <p className='text-white w-3/4 text-end font-semibold'>${product.price}</p>
-                <Link to={`/${product._id}/details`} className='w-full h-5 text-xs text-white rounded-lg text-center bg-[#053b50]'>See more</Link>
+                <div className='w-full flex gap-1'>
+                  <Link to={`/${product._id}/details`} className='w-full h-5 text-xs text-white rounded-lg text-center bg-[#053b50]'>See more</Link>
+                  <p onClick={()=>{
+                    if (!cart.find(item=> item.product._id === product._id)) {
+                       handleFavorite(product?._id)
+                    }
+                   }} className={`px-5 cursor-pointer h-5 text-xs text-white rounded-lg text-center ${(!cart.find(item=> item.product?._id === product._id))?"bg-[#053b50]": "bg-[#43626e56]" }`} >Add</p>
+                </div>
               </div>
             ))}
           </div>
